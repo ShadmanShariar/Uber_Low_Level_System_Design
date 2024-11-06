a99c3339eaec42043346401093a94aed4bb52e87
Shadman Shariar <shadmanshariar@Shadmans-MacBook-Pro.local>
2024-11-07 04:22:20 +0600

    swagger added, blob storage service modified

--- src/main/java/com/example/Uber/service/BlobStorageService.java
+++ src/main/java/com/example/Uber/service/BlobStorageService.java
@@ -3,9 +3,10 @@ package com.example.Uber.service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public interface BlobStorageService {
 
-    //String uploadProfilePic(String id, MultipartFile file) throws IOException;
+    String uploadLogo(UUID id, MultipartFile file) throws IOException;
 
 }
--- src/main/java/com/example/Uber/service/Implementation/BlobStorageServiceImpl.java
+++ src/main/java/com/example/Uber/service/Implementation/BlobStorageServiceImpl.java
@@ -1,30 +1,37 @@
 package com.example.Uber.service.Implementation;
 
+import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobContainerClientBuilder;
-import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.models.BlobHttpHeaders;
 import com.example.Uber.service.BlobStorageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class BlobStorageServiceImpl implements BlobStorageService {
-//    private final BlobContainerClient blobContainerClient;
-//    public BlobStorageServiceImpl(@Value("${azure.storage.connection-string}") String connectionString,
-//                                  @Value("${azure.storage.container-name}") String containerName) {
-//        this.blobContainerClient = new BlobContainerClientBuilder()
-//                .connectionString(connectionString)
-//                .containerName(containerName)
-//                .buildClient();
-//    }
-//    @Override
-//    public String uploadProfilePic(String id, MultipartFile file) throws IOException {
-//        String fileName = id + "_" + file.getOriginalFilename();
-//        BlobClient blobClient = blobContainerClient.getBlobClient(fileName);
-//        blobClient.upload(file.getInputStream(), file.getSize(), true);
-//        return blobClient.getBlobUrl();
-//    }
 
+    private final BlobContainerClient blobContainerClient;
+
+    public BlobStorageServiceImpl(@Value("${azure.storage.connection-string}") String connectionString,
+                                  @Value("${azure.storage.container-name}") String containerName) {
+        this.blobContainerClient = new BlobContainerClientBuilder()
+                .connectionString(connectionString)
+                .containerName(containerName)
+                .buildClient();
+    }
+
+    public String uploadLogo(UUID id, MultipartFile file) throws IOException {
+        String fileName = id + "_" + file.getOriginalFilename();
+        BlobClient blobClient = blobContainerClient.getBlobClient(fileName);
+        BlobHttpHeaders headers = new BlobHttpHeaders().setContentType(file.getContentType());
+        blobClient.upload(file.getInputStream(), file.getSize(), true);
+        blobClient.setHttpHeaders(headers);
+
+        return blobClient.getBlobUrl();
+    }
 }
